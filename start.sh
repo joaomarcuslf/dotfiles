a21ef1734ede87631429988b2933be1e2b9d5dbf
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2020-12-28 16:44:17 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,4 +10,22 @@ ln -sf `pwd`/.bash_aliases ~/.bash_aliases
 ln -sf `pwd`/.bash_exports ~/.bash_exports
 ln -sf `pwd`/.bashrc ~/.bashrc
 
+# Node and Ruby
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+curl -sSL https://get.rvm.io | bash -s stable --ruby --gems=rails,puma
+
+nvm install --lts
+
+# Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ln -sf `pwd`/zsh-themes/joaomarcuslf.zsh-theme ~/.oh-my-zsh/themes/joaomarcuslf.zsh-theme
+
+cd ${ZSH_CUSTOM1:-$ZSH/custom}/plugins && git clone https://github.com/djui/alias-tips.git
+cd ${ZSH_CUSTOM1:-$ZSH/custom}/plugins && git clone https://github.com/zsh-users/zsh-autosuggestions
+
+cd ~
+
+# Lein for Clojure
+curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein > lein
+sudo mv lein /usr/local/bin/lein
+sudo chmod a+x /usr/local/bin/lein
